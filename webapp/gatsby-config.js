9df6714a9b6349bef8d236cd2a035175ba000d92
Juicyjames <iceandjames@gmail.com>
2020-02-17 13:43:34 -0700

    added theming gatsby plugin

--- webapp/gatsby-config.js
+++ webapp/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Markdown Landing Page`,
+    description: `Write a landing page for anything`,
+    author: `@jamesanderegg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
