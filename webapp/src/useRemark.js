22e68e3b6c6a84a46297db99e1d2601f9e8e4dbb
Juicyjames <iceandjames@gmail.com>
2020-04-21 17:35:04 -0600

    uploading to github so zeit can see it

--- webapp/src/useRemark.js
+++ webapp/src/useRemark.js
@@ -2,21 +2,6 @@ import { useState, useEffect } from "react"
 import remark from "remark"
 import remark2react from "remark-react"
 
-// import codeScreenshot from "./remarkCodeScreenshot";
-// import urlThumbnail from "./remarkUrlThumbnail";
-// import twitterUserLinks from "./remarkTwitterUserLinks";
-// import { remarkGiphySearch, GiphySearch } from "./remarkGiphySearch";
-// import Screenshot from "../Screenshot";
-
-// import { githubLinks, GithubLink } from "./remarkGithubLinks";
-
-// const customHandler = type => (h, node) => {
-//   const props = {
-//     node,
-//   }
-
-//   return h(node, type, props)
-// }
 
 export const remarkCompile = input =>
   new Promise((resolve, reject) => {
